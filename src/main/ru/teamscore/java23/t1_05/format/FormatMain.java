54be14459b7f4180d4cf66e66315f1cbb69a973b
А.Коробецкая <kornast@yandex.ru>
2023-12-01 01:58:16 +0400

    Декомпозиция форматированного вывода таблицы

--- src/main/ru/teamscore/java23/t1_05/format/FormatMain.java
+++ src/main/ru/teamscore/java23/t1_05/format/FormatMain.java
@@ -1,5 +1,8 @@
 package ru.teamscore.java23.t1_05.format;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class FormatMain {
     public static void main(String[] args) {
         Item[] items = {
@@ -12,5 +15,29 @@ public class FormatMain {
         FormatTable table = new FormatTable(items);
 
         System.out.println(table);
+
+        Random rnd = new Random(1000);
+        Item[] itemsRandom = new Item[10000];
+        for (int i = 0; i <itemsRandom.length; i++) {
+            // название - случайные русские буквы
+            char[] charsRandom = new char[rnd.nextInt(5, 25)];
+            for (int ch = 0; ch < charsRandom.length; ch++) {
+                charsRandom[ch] = (char)rnd.nextInt('А', 'я');
+            }
+            itemsRandom[i] = new Item(
+                    new String(charsRandom),
+                    // гиперболическое распределение, чаще встречаются числа 1-50
+                    1 + rnd.nextInt(1, 10000) / rnd.nextInt(1, 2000),
+                    // экспоненциальное распределение, малые числа встречаются чаще больших
+                    Math.exp(rnd.nextDouble(0.1, Math.log(100_000))),
+                    // случайная дата за 2023-2024 годы
+                    LocalDate.of(2024, 12, 31)
+                            .minusDays(rnd.nextInt(365 * 2))
+            );
+        };
+
+        FormatTable tableRandom = new FormatTable(itemsRandom);
+
+        System.out.println(tableRandom);
     }
 }
--- src/main/ru/teamscore/java23/t1_05/format/FormatTable.java
+++ src/main/ru/teamscore/java23/t1_05/format/FormatTable.java
@@ -1,30 +1,99 @@
 package ru.teamscore.java23.t1_05.format;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.format.DateTimeFormatter;
 
 public class FormatTable {
+    private final Item[] items;
+
     public FormatTable(Item[] items) {
         this.items = items;
     }
 
-    private final Item[] items;
-
     @Override
     public String toString() {
-        var dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        StringBuilder sb = new StringBuilder();
-        String pattern = "│ %-16s │ %s │ %6d │ %9.2f │\n";
-        sb.append("┌──────────────────┬────────────┬────────┬───────────┐\n");
-        sb.append("│ Title            │ Deadline   │Quantity│ Price     │\n");
-        sb.append("├──────────────────┼────────────┼────────┼───────────┤\n");
-        for (Item item : items) {
-            sb.append(String.format(pattern,
+        var columnsTitles = new String[] {"Title", "Deadline", "Quantity", "Price"};
+        var columnsAlign = new boolean[]{ false, false, true, true };
+        return formatTable( getCells(), columnsAlign, columnsTitles );
+    }
+
+    final static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    final static NumberFormat quantityFormatter = new DecimalFormat("#,##0");
+    final static NumberFormat priceFormatter = new DecimalFormat("#,##0.00");
+    private String[][] getCells() {
+        var cells = new String[items.length][];
+        for (int r = 0; r < items.length; r++) {
+            var item = items[r];
+            var row = new String[] {
                     item.getTitle(),
                     dateFormatter.format(item.getDeadline()),
-                    item.getQuantity(),
-                    item.getPrice()));
+                    quantityFormatter.format(item.getQuantity()),
+                    priceFormatter.format(item.getPrice()),
+            };
+            cells[r] = row;
         }
-        sb.append("└──────────────────┴────────────┴────────┴───────────┘\n");
-        return sb.toString();
+        return cells;
+    }
+
+    private String formatTable(String[][] cells, boolean[] columnsAlignRight, String[] titleRow) {
+        var columnWidths = getColumnWidths(cells, titleRow);
+        String[] horizontalLines = getHorizontalLines(columnWidths);
+        String titleRowPattern = getRowFormat(columnWidths, new boolean[columnsAlignRight.length]);
+
+        String rowPattern = getRowFormat(columnWidths, columnsAlignRight);
+        var tableBuilder = new StringBuilder();
+        appendHorizontalLine(tableBuilder, horizontalLines, "┌", "┬", "┐");
+        appendFormattedRow(tableBuilder, titleRowPattern, titleRow);
+        appendHorizontalLine(tableBuilder, horizontalLines, "├", "┼", "┤");
+        for (String[] row : cells) {
+            appendFormattedRow(tableBuilder, rowPattern, row);
+        }
+        appendHorizontalLine(tableBuilder, horizontalLines, "└", "┴", "┘");
+        return tableBuilder.toString();
+    }
+
+    private static String[] getHorizontalLines(int[] columnWidths) {
+        String[] horizontalLines = new String[columnWidths.length];
+        for (int i = 0; i < columnWidths.length; i++) {
+            horizontalLines[i] = "─".repeat(columnWidths[i]).intern();
+        }
+        return horizontalLines;
+    }
+
+    private static void appendFormattedRow(StringBuilder tableBuilder, String pattern, String[] row) {
+        tableBuilder.append(String.format(pattern, (Object[]) row));
+    }
+
+    private static void appendHorizontalLine(StringBuilder tableBuilder, String[] horizontalLines,
+            String startWith, String delimiter, String endWith) {
+        tableBuilder
+                .append(startWith)
+                .append(String.join(delimiter, horizontalLines))
+                .append(endWith)
+                .append("\n");
+    }
+
+    private int[] getColumnWidths(String[][] cells, String[] titleRow) {
+        int[] maxTextLengths = new int[cells[0].length];
+        for (int c = 0; c < titleRow.length; c++) {
+            maxTextLengths[c] = titleRow[c].length();
+        }
+        for (String[] row : cells) {
+            for (int c = 0; c < row.length; c++) {
+                if (row[c].length() > maxTextLengths[c]) {
+                    maxTextLengths[c] = row[c].length();
+                }
+            }
+        }
+        return maxTextLengths;
+    }
+
+    private String getRowFormat(int[] columnWidths, boolean[] columnsAlignRight) {
+        String[] cellFormats = new String[columnWidths.length];
+        for (int c = 0; c < columnWidths.length; c++) {
+            cellFormats[c] = ("%" + columnWidths[c] * (columnsAlignRight[c] ? 1 : -1) + "s").intern();
+        }
+        return "│" + String.join("│", cellFormats) + "│\n";
     }
 }
